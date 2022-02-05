990b14af40714340d4907152747affd73090c56e
rahulshukla <rahul.shukla90@outlook.com>
2022-02-05 01:26:57 -0800

    Checking in code for 3 sum closest

---
+++ HomeWork2/3SumClosest/src/edu/northeastern/rahul/Main.java
@@ -0,0 +1,28 @@
+package edu.northeastern.rahul;
+
+import java.util.Arrays;
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int ans = -1;
+        for(int i = 0; i<nums.length-1; i++){
+            int p1=i+1, p2=nums.length-1;
+            while(p1<p2){
+                int sum = nums[i]+nums[p1]+nums[p2];
+                if (Math.abs(sum - target) < Math.abs(ans - target)) {
+                    ans = sum;
+                }
+
+                if (sum > target){
+                    p2--;
+                }
+                else if (sum < target) {
+                    p1++;
+                }
+                else return target;
+            }
+        }
+        return ans;
+    }
+}
