c7293e9fa41e74dc97a6ee537adee0bd4d7736fb
rahulshukla <rahul.shukla90@outlook.com>
2022-02-06 19:16:29 -0800

    Committing Code for Insert interval

--- HomeWork2/InsertInterval/src/edu/northeastern/rahul/Main.java
+++ HomeWork2/InsertInterval/src/edu/northeastern/rahul/Main.java
@@ -7,19 +7,19 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
 
         List<int[]> result = new ArrayList();
-        int i = 0, n = intervals.length;
-        while(i < n && intervals[i][1] < newInterval[0])
+        int i = 0;
+        while(i < intervals.length && intervals[i][1] < newInterval[0])
         {
             result.add(intervals[i++]);
         }
         int[] nI = newInterval;
-        while(i < n && intervals[i][0] <= newInterval[1]){
+        while(i < intervals.length && intervals[i][0] <= newInterval[1]){
             nI[0] = Math.min(nI[0], intervals[i][0]);
             nI[1] = Math.max(nI[1], intervals[i++][1]);
         }
         result.add(nI);
 
-        while(i < n) {
+        while(i < intervals.length) {
             result.add(intervals[i++]);
         }
 
