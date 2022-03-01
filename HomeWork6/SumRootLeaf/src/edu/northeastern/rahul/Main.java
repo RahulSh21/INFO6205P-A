c7fa91762c69d482ff80baac954e857a8d06f6b3
rahulshukla <rahul.shukla90@outlook.com>
2022-03-01 15:21:01 -0800

    Checking Code for Sum Root to Leaf Numbers

---
+++ HomeWork6/SumRootLeaf/src/edu/northeastern/rahul/Main.java
@@ -0,0 +1,41 @@
+package edu.northeastern.rahul;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int result = 0;
+    public int sumNumbers(TreeNode root){
+
+        sum(root, result);
+        return result;
+    }
+
+    public void sum(TreeNode root, int sum){
+        if (root == null)
+            return;
+        if (root.left == null && root.right == null) {
+
+            result += sum*10 + root.val;
+
+            return;
+        }
+
+        sum(root.left, sum *10  + root.val);
+        sum(root.right, sum *10  + root.val);
+    }
+
+}
