32051af8c4242ce0b0dc54a6c9f863926b7486e0
Rahul Shukla <90571552+RahulSh21@users.noreply.github.com>
2022-05-06 11:06:07 -0700

    Checking Code for Final

---
+++ Final Term/BinaryTree/src/edu/northeastern/rahul/Main.java
@@ -0,0 +1,16 @@
+package edu.northeastern.rahul;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > Math.max(p.val, q.val))
+            return lowestCommonAncestor(root.left, p, q);
+        if (root.val < Math.min(p.val, q.val))
+            return lowestCommonAncestor(root.right, p, q);
+        return root;
+    }
+}
