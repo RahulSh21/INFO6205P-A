f77e077b064881588537154c7800d8745481ae35
Rahul Shukla <90571552+RahulSh21@users.noreply.github.com>
2022-05-06 14:10:53 -0700

    Checking Code for Final

--- Final Term/Island/src/edu/northeastern/rahul/Main.java
+++ Final Term/Island/src/edu/northeastern/rahul/Main.java
@@ -9,25 +9,25 @@ public class Main {
 
 
         public int numIslands(char[][] grid) {
-            int ans = 0;
+            int result = 0;
 
             for (int i = 0; i < grid.length; ++i)
                 for (int j = 0; j < grid[0].length; ++j)
                     if (grid[i][j] == '1') {
-                        dfs(grid, i, j);
-                        ++ans;
+                        deptFirstSearch(grid, i, j);
+                        result++;
                     }
 
-            return ans;
+            return result;
         }
 
-        private void dfs(char[][] grid, int i, int j) {
+        private void deptFirstSearch(char[][] grid, int i, int j) {
             if (i < 0 || i == grid.length || j < 0 || j == grid[0].length)
                 return;
             if (grid[i][j] != '1')
                 return;
 
-            grid[i][j] = '2'; // mark '2' as visited
+            grid[i][j] = '2';
             dfs(grid, i + 1, j);
             dfs(grid, i - 1, j);
             dfs(grid, i, j + 1);
